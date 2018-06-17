c8dcc150503ebf2aa7e094573f5c277f7ac4ad26
PIAPAZI <arpiapazi@gmail.com>
2018-06-17 23:27:20 +0100

    bulletin update 2

--- app/src/main/java/com/example/randyp/bullentindesolde/SignupActivity.java
+++ app/src/main/java/com/example/randyp/bullentindesolde/SignupActivity.java
@@ -70,10 +70,6 @@ public class SignupActivity extends AppCompatActivity {
 
         _signupButton.setEnabled(false);
 
-
-
-        
-
         final ProgressDialog progressDialog = new ProgressDialog(SignupActivity.this);
         progressDialog.setIndeterminate(true);
         progressDialog.setMessage("Creating Account...");
